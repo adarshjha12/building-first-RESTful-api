5158ab53c0c80234d96c526c326296a00c2ac263
adarsh <jhaa50872@gmail.com>
2024-07-23 16:10:13 +0530

    server on!

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
 const express = require('express')
 const app = express()
+let port = process.env.port || 3000
+
+app.get('/', (req,res) =>{
+    res.send('helllo brother!!!!!!')
+})
+
+app.listen(port, () =>{
+    console.log(`running on ${port} `);
+})
